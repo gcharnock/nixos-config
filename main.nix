e22b9c1c4af04db5f62c61761b8c08f9706c05a3
System administrator <gareth.tpc@gmail.com>
2018-09-29 11:39:49 +0100

    Re add nixos binary cache

--- main.nix
+++ main.nix
@@ -23,10 +23,12 @@ in
 
   nix = {
     binaryCaches = [
+      "https://cache.nixos.org"
       "https://hie-nix.cachix.org"
     ];
     binaryCachePublicKeys = [
       "hie-nix.cachix.org-1:EjBSHzF6VmDnzqlldGXbi0RM3HdjfTU3yDRi9Pd0jTY="
+      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
     ];
   };
 
