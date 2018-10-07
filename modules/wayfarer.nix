359a5b67608e4ab91036a212c6b2637bb7e72827
System administrator <gareth.tpc@gmail.com>
2018-10-07 13:17:07 +0100

    Install autorandr

--- modules/wayfarer.nix
+++ modules/wayfarer.nix
@@ -5,6 +5,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
 
+  services.autorandr.enable = true;
+
   boot.initrd.luks.devices = [{
     name = "root";
     device = "/dev/sda2";
@@ -15,6 +17,10 @@
     ${pkgs.hdparm}/sbin/hdparm -Y /dev/sdb
     setxkbmap --options ctrl:nocaps
   '';
+
+  environment.systemPackages = with pkgs; [
+    autorandr
+  ];
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
