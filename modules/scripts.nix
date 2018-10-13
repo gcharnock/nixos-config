ef762a463cbc22ad782dfcb3aaf7b26f7004a842
System administrator <gareth.tpc@gmail.com>
2018-10-13 17:58:29 +0100

    Fix script

---
+++ modules/scripts.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+let setup-keyboard = pkgs.writeShellScriptBin "setup-keyboard" ''
+  setxkbmap -option "ctrl:nocaps";
+  ${pkgs.xorg.xmodmap}/bin/xmodmap -e "keycode 23=Hyper_L";
+  ${pkgs.xorg.xmodmap}/bin/xmodmap -e "keycode any=Tab";
+  ${pkgs.xorg.xmodmap}/bin/xmodmap -e "keycode 36=Hyper_R";
+  ${pkgs.xorg.xmodmap}/bin/xmodmap -e "keycode any=Return";
+  ${pkgs.xorg.xmodmap}/bin/xmodmap -e "add mod3 = Hyper_R";
+  ${pkgs.systemd}/bin/systemctl --user restart xcape;
+'';
+in
+{
+  environment.systemPackages = with pkgs; [
+    setup-keyboard
+  ];
+}
+
+
