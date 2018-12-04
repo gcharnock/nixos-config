5464bfd103064bf3011b4f60f7f7398aabaea6c2
Gareth Charnock <Gareth Charnock>
2018-12-04 10:36:10 +0000

    Anti-procrastination measures.

--- modules/symbinix.nix
+++ modules/symbinix.nix
@@ -14,6 +14,16 @@
     preLVM = true;
   }];
 
+  # I need to stop procrastinating. 
+  networking.hosts = {
+    "127.0.0.1" = [
+      "bbc.co.uk" 
+      "www.bbc.co.uk" 
+      "reddit.com" 
+      "www.reddit.com" 
+    ];
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
